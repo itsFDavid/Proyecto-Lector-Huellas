38c6f3f725837af115af758a74b612f00224f68f
itsFDavid <fdavid04@icloud.com>
2024-03-26 22:26:27 -0600

    Update

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,25 +1,38 @@
 const WebSocket = require('ws');
-const { SerialPort } = require('serialport');
+//const { SerialPort } = require('serialport');
 const fs = require('fs');
 
-const puertoSerie = new SerialPort({
+/*const puertoSerie = new SerialPort({
   path: 'COM3',
   baudRate: 9600,
 });
+*/
 
 const wss = new WebSocket.Server({ port: 8080 });
+
+let nextID=1;
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('Mensaje recibido del cliente:', message);
-
+    
     const data = JSON.parse(message);
-
+    data.id= nextID++;
     // Verificar el comando recibido y actuar en consecuencia
     switch (data.command) {
       case 'signUp':
-        guardarDatos(data);
+        const read= 'readingFootPrint';
+        ws.send(read, function(error){
+          if(error){
+            console.log('El mensaje no se pudo enviar');
+          }else{
+            console.log("Mensaje enviado el cual es: " + read)
+          }
+        })
+        
         //puertoSerie.write("Registrar Huella");
+
+        guardarDatos(data);
         break;
       default:
         console.log('Comando desconocido:', data.command);
@@ -47,7 +60,7 @@ wss.on('connection', function connection(ws) {
 // Función para agregar los datos recibidos al archivo existente
 function guardarDatos(datos) {
   // Ruta y nombre de archivo donde se guardarán los datos
-  const filePath = '../data/datos_cliente.json';
+  const filePath = '/Users/david/Desktop/Universidad/Semestre 4 Ubuntu/Metodos_Numericos/Proyecto/NodeJS/data/datos_cliente.json';
 
   // Leer los datos existentes del archivo, si los hay
   let datosAnteriores = [];
