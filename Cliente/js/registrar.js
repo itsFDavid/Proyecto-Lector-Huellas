a90c1a44e491a66657f512b2818b3fb82170eae4
itsFDavid <fdavid04@icloud.com>
2024-03-30 20:40:48 -0600

    Update

--- Cliente/js/registrar.js
+++ Cliente/js/registrar.js
@@ -31,6 +31,9 @@ document.addEventListener("DOMContentLoaded", function () {
       const reader = new FileReader();
       reader.onload = function (e) {
         const img = e.target.result;
+        ws.onmessage= function(event){
+          const res= event.data;
+        }
         const data = {
           command: "sigUp",
           name: name,
@@ -39,26 +42,19 @@ document.addEventListener("DOMContentLoaded", function () {
           photo: img,
           footPrint: "",
         };
-        const f={
-          command: 'pedirHuella'
-        }
         ws.send(JSON.stringify(data))
+        console.log("Aqui")
+        
+        modalLabel.textContent="Ingrese huella"
+        miModal.show()
+        btnOkey.addEventListener('click', function(){
+          console.log('Huella capturada')
+          window.location.href="../index.html"
+        })
       };
       reader.readAsDataURL(filePhoto.files[0]);
     }
   });
-
-  ws.onmessage = function (event) {
-    const res = event.data;
-    modalLabel.textContent = "Onmessage"; // Actualiza contenido del modal
-    miModal.handleUpdate(); // Actualiza el modal
-    miModal.show();
-    console.log("Mensaje recibido del servidor:", event.data);
-    console.log(res);
-
-    // Agrega un retraso si es necesario
-    // setTimeout(miModal.show, 100);
-  };
 
   const cancel = document.getElementById("cancel");
   cancel.addEventListener("click", function () {
