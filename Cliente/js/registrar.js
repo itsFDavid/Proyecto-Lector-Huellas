2bc61bd614a37c0fde0b21a4ed65d3a8373681d0
itsFDavid <fdavid04@icloud.com>
2024-04-03 22:01:49 -0600

    Update

--- Cliente/js/registrar.js
+++ Cliente/js/registrar.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
   var miModal = new bootstrap.Modal(document.getElementById("exampleModal"));
   var modalLabel = document.getElementById("exampleModalLabel");
   const btnOkey = document.getElementById("btn-okey");
-
+  let ws;
+  ws = new WebSocket('ws://localhost:8080');
   if (conectado === "false" || conectado === undefined) {
     modalLabel.textContent =
       "No hay conexión con el servidor. Por favor, intenta de nuevo más tarde.";
@@ -19,14 +20,20 @@ document.addEventListener("DOMContentLoaded", function () {
   capturarHuella.addEventListener('click', ()=>{
     modalLabel.textContent="Ingresa huella en el lector, mantenla ahi";
     miModal.show()
-    btnOkey.addEventListener('click', ()=>{
+    ws.send(JSON.stringify({command: "signUp"}))
+    /*btnOkey.addEventListener('click', ()=>{
+      window.location.href="../pages/registrar_huella.html"
+    });
+    if(res=='ok'){
       modalLabel.textContent="Huella registrada";
       miModal.show();
       btnOkey.addEventListener('click', ()=>{
         window.location.href="../index.html";
       });
-    });
+    }
+    */
   });
+
     const cancel = document.getElementById("cancel");
     cancel.addEventListener("click", function () {
       window.location.href = "../index.html";
