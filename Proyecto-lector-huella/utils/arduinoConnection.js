bb6c364f898dde4d76be75e40380dd4db30279f6
itsFDavid <fdavid04@icloud.com>
2024-04-09 20:43:51 -0600

    Create conecction to arduino and more utilities for the project

---
+++ Proyecto-lector-huella/utils/arduinoConnection.js
@@ -0,0 +1,16 @@
+
+const { SerialPort } = require('serialport');
+const Readline = require('@serialport/parser-readline');
+
+
+const arduinoPort = new SerialPort({
+    path: '/dev/cu.usbserial-1130',
+    baudRate: 9600,
+});
+
+// Configurar el parser para leer los datos del puerto serie
+const parser = arduinoPort.pipe(new Readline.ReadlineParser({ delimiter: '\n' }));
+
+
+
+module.exports = { arduinoPort, parser};
---
+++ Proyecto-lector-huella/utils/data.js
@@ -0,0 +1,69 @@
+const allData = 
+{ users:[
+    {
+      nombre: "Pedro",
+      edad: 19,
+      carrera: "Ing. Sistemas",
+      huella_id: 1
+    },
+    {
+      nombre: "María",
+      edad: 22,
+      carrera: "Ing. Industrial",
+      huella_id: 2
+    },
+    {
+      nombre: "Juan",
+      edad: 20,
+      carrera: "Lic. Administración",
+      huella_id: 3
+    },
+    {
+      nombre: "Ana",
+      edad: 21,
+      carrera: "Medicina",
+      huella_id: 4
+    },
+    {
+      nombre: "Carlos",
+      edad: 23,
+      carrera: "Derecho",
+      huella_id: 5
+    },
+    {
+      nombre: "Sofía",
+      edad: 24,
+      carrera: "Psicología",
+      huella_id: 6
+    },
+    {
+      nombre: "Luis",
+      edad: 25,
+      carrera: "Arquitectura",
+      huella_id: 7
+    },
+    {
+      nombre: "Laura",
+      edad: 26,
+      carrera: "Biología",
+      huella_id: 8
+    },
+    {
+      nombre: "Javier",
+      edad: 27,
+      carrera: "Química",
+      huella_id: 9
+    },
+    {
+      nombre: "Carmen",
+      edad: 28,
+      carrera: "Literatura",
+      huella_id: 10
+    }
+  ]
+};
+
+
+module.exports={
+    allData
+}  
