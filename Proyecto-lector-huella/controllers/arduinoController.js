c7475d4244df7682f91d9d079221d51fe976dc7d
itsFDavid <fdavid04@icloud.com>
2024-04-10 19:46:57 -0600

    Update logic

--- Proyecto-lector-huella/controllers/arduinoController.js
+++ Proyecto-lector-huella/controllers/arduinoController.js
@@ -1,5 +1,6 @@
 const { arduinoPort } = require('../utils/arduinoConnection');
 const allData = require('../utils/data');
+const { dataTMP }= require('../utils/dataTMP');
 
 
 
@@ -13,30 +14,43 @@ const getIdFingers= (req, res)=>{
 
 }
 
-const signUp = (req, res) => {
+const signUp = async (req, res) => {
     const data = req.body;
+    const urlGetData= `http://localhost:4321/api/arduino/getDataUser/${data.huellas_id}`;
 
+    const response = await fetch(urlGetData);
+    const dataUser = await response.json();
+    console.log('Datos del usuario:', dataUser);
+
+    console.log('Datos del usuario:', data.huellas_id);
+    console.log('ID del usuario:', dataUser.response);
     if (!data || data == null || data == undefined || data == '') {
         res.status(400).json({ response: 'error', message: 'Los campos son requeridos' });
-    } else {
-        const jsonData = JSON.stringify(data);
+    } else if(data.id<0){
+        res.status(400).json({ response: 'error', message: 'El id no puede ser menor a 0' });
+    }else if(dataUser.response=='ok'){
+        res.status(400).json({ response: 'error', message: 'El id ya existe'});
+    }else {
+        const { nombre, edad, carrera, huella_id, fotoUser, command } = data;
+        const userDataTmp = new dataTMP();
+        userDataTmp.addData({nombre: nombre, edad: edad, carrera: carrera, huella_id: huella_id, fotoUser: fotoUser});
+
+        const jsonData = JSON.stringify({command: command, huella_id: huella_id});
         arduinoPort.write(jsonData);
         console.log('Comando enviado al Arduino:', jsonData);
         res.json({ response: 'ok', message: `Enviado correctamente.` });
     }
 };
 
-module.exports = {
-    signUp
-};
+
 
 const deleteFinger = (req, res) => {
-    const data = req.body;
-    console.log('Comando enviado al Arduino:', data);
-    if (!data || data == null) {
+    const {id} = req.params;
+    console.log('id enviado a eliminar al Arduino:', id);
+    if (!id || id == null) {
         res.status(400).json({ response: 'error', message: 'Los campos son requerido' });
     } else {
-        const jsonData = JSON.stringify(data);
+        const jsonData = JSON.stringify({ command: 'deleteFinger', huella_id: id });
         arduinoPort.write(jsonData);
         res.json({ response: 'ok', message: `Enviado correctamente.` });
     }
