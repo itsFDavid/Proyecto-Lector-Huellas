170ff0e47cf69f2dab5dca08d2d32e5ae9ac32ab
itsFDavid <fdavid04@icloud.com>
2024-04-11 23:22:27 -0600

    update websockets

--- Proyecto-lector-huella/app.js
+++ Proyecto-lector-huella/app.js
@@ -5,8 +5,8 @@ const WebSocket = require('ws'); // Importa el módulo de WebSockets
 const arduinoRouter = require('./routes/arduinoRouter');
 const { parser } = require('./utils/arduinoConnection');
 const { dataTMP } = require('./utils/dataTMP');
-
-
+const {insertar, obtenerNumeroIds} = require('./models/modelBD');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Configurar middleware de CORS
@@ -14,7 +14,8 @@ app.use(cors({
     origin: 'http://127.0.0.1:5500',
     methods: ['GET', 'DELETE', 'HEAD', 'OPTIONS', 'POST', 'PUT'],
 }));
-
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 const port = 4321;
 const server = http.createServer(app);
 
@@ -35,9 +36,11 @@ wss.on('connection', (ws) => {
 });
 
 parser.on('data', async (data) => {
+    let jsonDatas;
     let jsonData;
     try {
         jsonData = JSON.parse(data);
+
         // Enviar datos al cliente a través de WebSocket
         console.log('Datos recibidos del Arduino:', jsonData);
         if(jsonData.found){
@@ -45,34 +48,64 @@ parser.on('data', async (data) => {
 
             const response = await fetch(urlGetData);
             const dataUser = await response.json(); 
-            console.log('Datos del usuario:', dataUser);
+            console.log('Datos del usuario para login:', dataUser);
 
             jsonData = dataUser;
+            wss.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({ event: 'found', data: jsonData }));
+                }
+            });
+            //console.log('Datos enviados al cliente en login:', jsonData);
 
+        }else if(jsonData.numFootprints){
+            console.log('Datos recibidos del num:', jsonData);
+            const response = await obtenerNumeroIds();
+            const numFootprintsBD = response[0].numFootprints;
+            console.log('Datos de la base de datos:', numFootprintsBD);
+            const numFootprintsArduino = jsonData.numFootprints;
+            if(numFootprintsBD == numFootprintsArduino){
+                jsonData = {message: `Los datos en la base de datos y arduino son: ${numFootprintsBD}`};
+            }else{
+                jsonData = {message: `Los datos en la base de datos y arduino no coinciden: ${numFootprintsBD} y ${numFootprintsArduino} respectivamente`};
+            }
+            wss.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({ event: 'numFootprints', data: jsonData }));
+                    console.log('Datos enviados al cliente:', jsonData);
+                }
+            });
+            
         }
     } catch (error) {
         const cleanedData = data.trim();
         // Si falla el análisis, convertir los datos a JSON
+       
         jsonData = { message: cleanedData }; 
+        //console.log('Datos recibidos del Arduinoss:', jsonData);
         console.log('Datos recibidos del Arduinos:', jsonData);
-        if(jsonData.message== "Huella guardada"){
+        if(jsonData.message== "Huella guardada"){ 
             //guardar los datos en data
             const userDataTmp = new dataTMP();
-            const dataUser = userDataTmp.getData();
+            const dataUser = userDataTmp.getData()[0];
             console.log('Datos del usuario:', dataUser);
-            userDataTmp.deleteData();
+            //insertar en la base de datos tipo estos datos:
 
-            console.log('Datos recibidos del Arduinolos:', dataUser);     
+            const {nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, carrera, correoInstitucional, fotoUser} = dataUser;
+            console.log('Datos del usuario:', nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, carrera, correoInstitucional, fotoUser);
+
+            const response = await insertar(nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, fotoUser, carrera, correoInstitucional);
+            console.log('Datos insertados:', response);
+            userDataTmp.deleteData();
         }
     }
-    // Enviar datos al cliente a través de WebSocket
-
     let JsonString= JSON.stringify(jsonData);
     wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
             client.send(JsonString);
         }
     });
+    console.log('Datos enviados al cliente:', JsonString);
 });
 
 
