ccbcaacac4697db6f284553e146cf7e963d15863
itsFDavid <fdavid04@icloud.com>
2024-04-11 23:24:00 -0600

    add dependencies

---
+++ Proyecto-lector-huella/models/modelBD.js
@@ -0,0 +1,84 @@
+const conexion = require("../utils/dbConection");
+
+module.exports = {
+    insertar(nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, fotoUser, carrera, correoInstitucional) {
+        return new Promise((resolve, reject) => {
+            conexion.query(`
+                INSERT INTO usuarios 
+                (nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, fotoUser, carrera, correoInstitucional) 
+                VALUES (?, ?, ?, ?, ?, ?, ?)`,
+                [nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, fotoUser, carrera, correoInstitucional],
+                (err, resultados) => {
+                    if (err) reject(err);
+                    else resolve(resultados.insertId);
+                }
+            );
+        });
+    },
+    obtener() {
+        return new Promise((resolve, reject) => {
+            conexion.query(`
+                SELECT id_huella, nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, fotoUser, carrera, correoInstitucional 
+                FROM usuarios`,
+                (err, resultados) => {
+                    if (err) reject(err);
+                    else resolve(resultados);
+                }
+            );
+        });
+    },
+    obtenerPorId(id_huella) {
+        return new Promise((resolve, reject) => {
+            conexion.query(`
+                SELECT id_huella, nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, fotoUser, carrera, correoInstitucional 
+                FROM usuarios 
+                WHERE id_huella = ?`,
+                [id_huella],
+                (err, resultados) => {
+                    if (err) reject(err);
+                    else resolve(resultados[0]);
+                }
+            );
+        });
+    },
+    eliminar(id_huella) {
+        return new Promise((resolve, reject) => {
+            conexion.query(`
+                DELETE FROM usuarios 
+                WHERE id_huella = ?`,
+                [id_huella],
+                (err) => {
+                    if (err) reject(err);
+                    else resolve(`Eliminado correctamente en bd: ${id_huella} `);
+                }
+            );
+        });
+    },
+    obtenerUltimoId() {
+        return new Promise((resolve, reject) => {
+            conexion.query(`
+                SELECT MAX(id_huella) AS ultimoId 
+                FROM usuarios`,
+                (err, resultados) => {
+                    if (err) reject(err);
+                    else {
+                        const ultimoId = resultados[0].ultimoId !== null ? resultados[0].ultimoId : 0;
+                        resolve(ultimoId);
+                    }
+                }
+            );
+        });
+    },    
+    obtenerNumeroIds() {
+        return new Promise((resolve, reject) => {
+            conexion.query(`
+                SELECT COUNT(id_huella) AS totalIds 
+                FROM usuarios`,
+                (err, resultados) => {
+                    if (err) reject(err);
+                    else resolve(resultados[0].totalIds);
+                }
+            );
+        });
+    }
+};
