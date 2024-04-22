c566d11c935667e834019ab107d780e41415000f
itsFDavid <fdavid04@icloud.com>
2024-04-22 11:14:08 -0600

    update how to show all data

--- Proyecto-lector-huella/front/js/all.js
+++ Proyecto-lector-huella/front/js/all.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", async ()=>{
     const conectado = localStorage.getItem("conectado");  
   var miModal = new bootstrap.Modal(document.getElementById("exampleModal"));
   var modalLabel = document.getElementById("exampleModalLabel");
@@ -15,8 +15,36 @@ document.addEventListener("DOMContentLoaded", ()=>{
     });
     return;
   }
- // TODO: hacer que muestre los datos registrados en arduino independientemente si hay o no
-  ws.onmessage= function(message){
-    const data= message
+  const response= await fetch('http://localhost:4321/api/arduino/getAllData')
+  const data= await response.json()
+  
+  const tabla= document.getElementById('tabla-huellas')
+  let tr;
+  let th_nombre, th_edad, th_carrera, th_idhuella, th_correo;
+  for(let i=0; i<data.length; i++){
+    tr= document.createElement('tr')
+    th_nombre= document.createElement('th')
+    th_edad= document.createElement('th')
+    th_carrera= document.createElement('th')
+    th_idhuella= document.createElement('th')
+    th_correo= document.createElement('th')
+    th_nombre.innerHTML= data[i].nombre
+    th_edad.innerHTML= data[i].fechaNacimiento
+    th_carrera.innerHTML= data[i].carrera
+    th_idhuella.innerHTML= data[i].id_huella
+    th_correo.innerHTML= data[i].correoInstitucional
+
+    tr.appendChild(th_idhuella)
+    tr.appendChild(th_nombre)
+    tr.appendChild(th_edad)
+    tr.appendChild(th_carrera)
+    tr.appendChild(th_correo)
+    tabla.appendChild(tr)
   }
+  
+  const back= document.getElementById('back')
+  back.addEventListener('click', (e)=>{
+    e.preventDefault()
+    window.location.href= "../index.html"
+  })
 })
