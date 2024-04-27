084d03aac3c640b040a4ea6899dcb59527404a1f
itsFDavid <fdavid04@icloud.com>
2024-04-26 23:51:02 -0600

    upload multer to upload photos

--- Proyecto-lector-huella/routes/arduinoRouter.js
+++ Proyecto-lector-huella/routes/arduinoRouter.js
@@ -3,12 +3,25 @@
 const express = require('express');
 const arduinoController = require('../controllers/arduinoController');
 const multer = require('multer');
+const path = require('path');
 
 const router = express.Router();
-const upload = multer({destination: '../uploadsImages/'});
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      const uploadPath = path.join(__dirname, '../public/uploads');
+      cb(null, uploadPath)
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + `.${file.originalname.split('.').pop()}`)
+    }
+  })
+  
+const uploadTemp = multer();
+const upload = multer({ storage: storage })
 
 // Ruta para enviar datos al Arduino
-router.post('/register', arduinoController.signUp);
+router.post('/register', upload.single('fotoUser'), arduinoController.signUp);
 
 // Ruta para obtener datos del Arduino
 router.get('/getFingers', arduinoController.getIdFingers);
@@ -21,5 +34,8 @@ router.get('/verifyFinger', arduinoController.logIn);
 
 router.get('/getDataUser/:id', arduinoController.getDataUser);
 
+//router.post('/uploadImage', upload.single('image'), arduinoController.uploadImage);
+
+router.get('/getImages/:image', arduinoController.getImages);
 
 module.exports = router;
